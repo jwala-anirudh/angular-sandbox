7abbd4fe8137a1990e9eddc880496a1a5c14a153
Anirudh Jwala <anirudhsai2@gmail.com>
2024-12-14 02:37:06 +0530

    feat: add cart component with item listing and order summary

---
+++ src/app/pages/cart/cart.component.ts
@@ -0,0 +1,13 @@
+import { Component, inject } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { CartItemComponent } from './cart-item/cart-item.component';
+import { OrderSummaryComponent } from './order-summary/order-summary.component';
+
+@Component({
+  selector: 'app-cart',
+  imports: [CartItemComponent, OrderSummaryComponent],
+  templateUrl: './cart.component.html',
+})
+export class CartComponent {
+  cartService = inject(CartService);
+}
